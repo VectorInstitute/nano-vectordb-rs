570730b5a2e10e1e7421fa34799e16bcc149b514
Amrit Krishnan <amrit110@gmail.com>
2025-02-08 00:03:08 -0500

    Add clippy lint, fixes

--- src/lib.rs
+++ src/lib.rs
@@ -50,10 +50,7 @@ mod base64_bytes {
 
     pub fn serialize<S: Serializer>(vec: &[Float], serializer: S) -> Result<S::Ok, S::Error> {
         let bytes = unsafe {
-            std::slice::from_raw_parts(
-                vec.as_ptr() as *const u8,
-                vec.len() * std::mem::size_of::<Float>(),
-            )
+            std::slice::from_raw_parts(vec.as_ptr() as *const u8, std::mem::size_of_val(vec))
         };
         let b64 = general_purpose::STANDARD.encode(bytes);
         serializer.serialize_str(&b64)
@@ -92,15 +89,25 @@ impl Eq for ScoredIndex {}
 
 impl PartialOrd for ScoredIndex {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.score.partial_cmp(&other.score)
+        Some(self.cmp(other))
     }
 }
 
 impl Ord for ScoredIndex {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap_or(Ordering::Equal)
+        self.score.partial_cmp(&other.score).unwrap_or_else(|| {
+            if self.score.is_nan() && other.score.is_nan() {
+                Ordering::Equal
+            } else if self.score.is_nan() {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        })
     }
 }
+
+type DataFilter = Box<dyn Fn(&Data) -> bool + Send + Sync>;
 
 impl NanoVectorDB {
     /// Creates a new NanoVectorDB instance
@@ -181,7 +188,7 @@ impl NanoVectorDB {
         query: &[Float],
         top_k: usize,
         better_than: Option<Float>,
-        filter: Option<Box<dyn Fn(&Data) -> bool + Send + Sync>>,
+        filter: Option<DataFilter>,
     ) -> Vec<HashMap<String, serde_json::Value>> {
         let query_norm = normalize(query);
         let embedding_dim = self.embedding_dim;
@@ -354,7 +361,6 @@ pub fn normalize(vector: &[Float]) -> Vec<Float> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json;
     use std::collections::HashMap;
     use tempfile::NamedTempFile;
 
